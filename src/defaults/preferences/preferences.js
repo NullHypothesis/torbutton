a882773508088d2096dcb7960cfd3b8b15cbf6e0
Mike Perry <mikeperry-git@fscked.org>
2007-09-20 21:26:05 +0000

    It helps to get the bug numbers right. Also, setting the loglevel to 4 in SVN because otherwise I just know I will forget to raise it for some release.
    svn:r11541

--- src/defaults/preferences/preferences.js
+++ src/defaults/preferences/preferences.js
@@ -1,6 +1,6 @@
 // pref("extensions.torbutton.prompt_before_visiting_excluded_sites",true);
 pref("extensions.torbutton.debug",true);
-pref("extensions.torbutton.loglevel",1);
+pref("extensions.torbutton.loglevel",4);
 pref("extensions.torbutton.display_panel",true);
 pref("extensions.torbutton.panel_style",'text');
 pref("extensions.torbutton.settings_method",'recommended');
