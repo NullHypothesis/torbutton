61e9545c86543769e492e6a65a0ef1be9b541311
Mike Perry <mikeperry-git@fscked.org>
2008-01-24 05:47:16 +0000

    Add design document to SVN. Can't hide it in the dark forever.
    svn:r13255

---
+++ website/design/build.sh
@@ -0,0 +1,1 @@
+xsltproc  --output design.html.en  --stringparam section.autolabel.max.depth 2 --stringparam  section.autolabel 1 /usr/share/sgml/docbook/xsl-stylesheets-1.73.2/xhtml/docbook.xsl design.xml 
