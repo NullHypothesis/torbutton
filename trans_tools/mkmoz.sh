f42a67c54949859e2f6b293798dffd82cc9cd206
Mike Perry <mikeperry-git@fscked.org>
2008-06-08 20:02:27 +0000

    Move some translation scripts and patches from the locale directory to the trans_tools directory.
    svn:r15042

--- trans_tools/mkmoz.sh
+++ trans_tools/mkmoz.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 #
 
+LOCALEDIR="../src/chrome/locale/"
+
 directories="ar de-DE es hr-HR nl-NL pt-BR sl-SI de-AT el-GR fa-IR it-IT pl-PL zh-CN de-CH fr-FR ru zh-TW"
 poDir="po" 
 mozDir="moz"
@@ -11,6 +13,6 @@ for dir in $directories
 do
     pootleDir="`echo $dir|tr - _`";
     mkdir -p $mozDir/$dir/
-    po2moz -i $poDir/$pootleDir/ -t $input/ -o $mozDir/$dir/ 
+    po2moz -i $poDir/$pootleDir/ -t ${LOCALEDIR}/${input}/ -o $mozDir/$dir/ 
 done
 
--- trans_tools/mkpo.sh
+++ trans_tools/mkpo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 #
 
+LOCALEDIR="../src/chrome/locale/"
+
 directories="ar de-DE es hr-HR nl-NL pt-BR sl-SI de-AT el-GR fa-IR it-IT pl-PL zh-CN de-CH fr-FR ru zh-TW"
 outdir="po"
 #input="en-US/torbutton.dtd"
@@ -12,5 +14,5 @@ do
     pootleDir="`echo $dir|tr - _`";
     mkdir -p $outdir/$pootleDir/
     #moz2po -i $dir/$template -t $input -o $outdir/$pootleDir/torbutton.po
-    moz2po -i $dir/ -t $input/ -o $outdir/$pootleDir/
+    moz2po -i $LOCALEDIR/$dir/ -t $LOCALEDIR/$input/ -o $outdir/$pootleDir/
 done
