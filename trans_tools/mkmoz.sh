661e9037e6192cd9892300945c84d6ab2d24c66d
Mike Perry <mikeperry-git@fscked.org>
2009-03-16 03:52:58 +0000

    Change mkmoz script to work with arbitrary new countries.
    svn:r19033

--- trans_tools/mkmoz.sh
+++ trans_tools/mkmoz.sh
@@ -3,8 +3,8 @@
 
 LOCALEDIR="../src/chrome/locale"
 
-directories="ar de-DE es hr-HR nl-NL pt-BR sl-SI de-AT el-GR fa-IR it-IT pl-PL zh-CN de-CH fr-FR ru zh-TW"
 poDir="pootle" 
+directories="`ls -1 ./$poDir|tr _ -`"
 mozDir="moz"
 input="en-US"
 template="torbutton.dtd"
@@ -12,7 +12,9 @@ template="torbutton.dtd"
 for dir in $directories
 do
     pootleDir="`echo $dir|tr - _`";
+    echo "$pootleDir"
     mkdir -p $mozDir/$dir/
     po2moz -i $poDir/$pootleDir/ -t ${LOCALEDIR}/${input}/ -o $mozDir/$dir/ 
+    #po2moz -i $poDir/$pootleDir/ -t pootle/templates/ -o $mozDir/$dir/ 
 done
 
