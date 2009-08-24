2efdcabc74cf601603991d4d59ce69cf5189fff6
Mike Perry <mikeperry-git@fscked.org>
2009-08-24 03:20:26 +0000

    Partially update design doc for 1.2.2 and note several places that need more work. Also include a list of known anonymity testing pages.
    svn:r20356

--- website/design/build.sh
+++ website/design/build.sh
@@ -1,1 +1,1 @@
-xsltproc  --output index.html.en  --stringparam section.autolabel.max.depth 2 --stringparam  section.autolabel 1 /usr/share/sgml/docbook/xsl-stylesheets--1.73.2/xhtml/docbook.xsl design.xml 
+xsltproc  --output index.html.en  --stringparam section.autolabel.max.depth 2 --stringparam  section.autolabel 1 /usr/share/sgml/docbook/xsl-stylesheets-1.74.0/xhtml/docbook.xsl design.xml 
