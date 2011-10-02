NullHypothesis/torbutton

commit 84e8dbc93a59c5d60e472628f1fbfc45713fe569

Properly use nsIPrefLocalizedString in torbutton_get_general_useragent_locale()
Replace the custom dereferencing of the stringbundle by the proper usage of
nsIPrefLocalizedString.

src/chrome/content/torbutton.js
@@ -1238,15 +1238,8 @@ function torbutton_set_timezone(mode, startup) {
 
 function torbutton_get_general_useragent_locale() {
    try {
-        var locale = m_tb_prefs.getCharPref("general.useragent.locale");
-        if (locale != "chrome://global/locale/intl.properties") {
-            return locale;
-        }
-
-        var bundle = Components.classes["@mozilla.org/intl/stringbundle;1"]
-                                .getService(Components.interfaces.nsIStringBundleService);
-        var stringbundle = bundle.createBundle(locale);
-        return stringbundle.GetStringFromName("general.useragent.locale");
+        return m_tb_prefs.getComplexValue("general.useragent.locale",
+                   Components.interfaces.nsIPrefLocalizedString).data;
     } catch(err) {
         torbutton_log(4, "Error while getting general.useragent.locale:" + err);
         return 'en-US';

commit 74a816cad78fc360e95ecc3cb9574f0997acef09

Use nsIPrefLocalizedString only when required in torbutton_get_general_useragent_locale()
Otherwise, Firefox throws an uncatcheable NS_ERROR_UNEXPECTED exception.
At least in TBB. Strange, strange.

Fix issue introduced in 84e8dbc9.

src/chrome/content/torbutton.js
@@ -1222,8 +1222,12 @@ function torbutton_set_timezone(mode, startup) {
 
 function torbutton_get_general_useragent_locale() {
    try {
-        return m_tb_prefs.getComplexValue("general.useragent.locale",
-                   Components.interfaces.nsIPrefLocalizedString).data;
+        var locale = m_tb_prefs.getCharPref("general.useragent.locale");
+        if (/chrome:\/\//.test(locale)) {
+            return m_tb_prefs.getComplexValue("general.useragent.locale",
+                       Components.interfaces.nsIPrefLocalizedString).data;
+        }
+        return locale;
     } catch(err) {
         torbutton_log(4, "Error while getting general.useragent.locale:" + err);
         return 'en-US';