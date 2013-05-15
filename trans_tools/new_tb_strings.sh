955dc4e56c0d9072179d30ec8bc7696e655bcaae
Mike Perry <mikeperry-git@fscked.org>
2013-05-14 17:38:35 -0700

    Add browser branding files to trans_tools merge script.

--- trans_tools/new_tb_strings.sh
+++ trans_tools/new_tb_strings.sh
@@ -6,6 +6,8 @@ for i in `ls -1 po`
 do
    msgmerge -U ./po/$i/torbutton.dtd.po ./po/templates/torbutton.dtd.pot
    msgmerge -U ./po/$i/torbutton.properties.po ./po/templates/torbutton.properties.pot
+   msgmerge -U ./po/$i/browser.dtd.po ./po/templates/browser.dtd.pot
+   msgmerge -U ./po/$i/browser.properties.po ./po/templates/browser.properties.pot
 done
 
 svn diff po
